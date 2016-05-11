22723774e798729b3cc2684f0c7c3ea463077fa7
Antonio José Navarro Céspedes <ajncespedes@gmail.com>
2016-05-11 23:52:01 +0200

    Añadidos cambios
    Añadidos ligeros cambios en el diseño 3d y añadido sistema de luz por
    tiempo.

---
+++ Assets/Scripts/luces.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class luces : MonoBehaviour
+{
+    GameObject[] lightGameObjects = null;
+    Light[] myLight = null;
+    GameObject[] target = null;
+    float timeLeft;
+    AudioSource audio;
+
+
+    void Start()
+    {
+        timeLeft = 120;
+        target = GameObject.FindGameObjectsWithTag("interruptor");
+        lightGameObjects = GameObject.FindGameObjectsWithTag("luz");
+        //private Light myLight;
+        myLight = new Light[lightGameObjects.Length];
+        for (int i = 0; i < lightGameObjects.Length; i++)
+        {
+            myLight[i] = lightGameObjects[i].GetComponent<Light>();
+        }
+        
+        //myLight = GetComponent<Light>();
+    }
+
+   
+
+    void Update()
+    {
+        
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            for (int j = 0; j < lightGameObjects.Length; j++)
+            {
+                myLight[j].enabled = false;
+            }
+        }
+        for (int i = 0; i < target.Length; i++)
+        {
+            float distance = Vector3.Distance(transform.position, target[i].transform.position);
+            if (distance < 5)
+            {
+                if (Input.GetKeyUp(KeyCode.F))
+                {
+                    audio = target[i].GetComponent<AudioSource>();
+                    audio.enabled = true;
+                    audio.Play();
+                    for (int j = 0; j < lightGameObjects.Length; j++)
+                    {
+                        myLight[j].enabled = true;
+                    }
+                    timeLeft = 120;
+                }
+            }
+        }
+        Debug.Log("Tiempo restante: " + timeLeft);
+    }
+}
---
+++ Assets/Scripts/luz.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class luz : MonoBehaviour {
+
+    private Light myLight;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
